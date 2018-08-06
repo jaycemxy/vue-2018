9145e1b7c1aefb25f4b6180a83ccbdae85e52c67
jaycemxy <jayce_ma.xa@foxmail.com>
2018-08-06 22:33:50 +0800

    邮箱验证

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # vue-2018
+## 验证用户（邮箱）是否存在
+在methods中有一个onLogin方法，当你填入的邮箱不存在时，error.code === 211
+![alt text](https://i.loli.net/2018/08/06/5b6856e2bc2f3.png)
+
+当你填入的邮箱名与密码不匹配时，error.code === 210
+![alt text](https://i.loli.net/2018/08/06/5b6856e9bd28f.png)
+
+当输入的邮箱和密码都正确时，leancloud会将用户信息储存在sessionID中
+![alt text](https://i.loli.net/2018/08/06/5b68595c6d9f7.png)
+
+AV对象通过响应头将sessionID传给JS，JS又会将sessionID存入localStorage从而模拟了cookie
+![alt text](https://i.loli.net/2018/08/06/5b685ad4c987b.png)
--- src/js/app.js
+++ src/js/app.js
@@ -10,11 +10,40 @@ let app = new Vue({
             phone: '182xxxxxxxx',
             email: 'example@example.com',
             github: ' https://github.com/'
-        }
+        },
+        login: {
+            email: '',
+            password: ''
+        },
+        signUp: {
+            email: '',
+            password: ''
+        },
     },
     methods: {
         onEdit(key, value) {
             this.resume[key] = value
+        },
+        onLogin(e){
+            console.log(this.login)
+             AV.User.logIn(this.login.email, this.login.password).then(function (user) {
+                 console.log(user)
+             }, function (error) {
+                if(error.code === 211){
+                    alert('邮箱不存在')
+                }else if(error.code === 210){
+                    alert('用户名和密码不匹配')
+                }
+             })
+        },
+        onSignUp(e){
+            const user = new AV.User()
+            user.setUsername(this.signUp.email)
+            user.setPassword(this.signUp.password)
+            user.setEmail(this.signUp.email)
+            user.signUp().then(function (User) {
+                console.log(user)
+            }, function (error) {})
         },
         onClickSave(){
             let currentUser = AV.User.current()
@@ -23,12 +52,6 @@ let app = new Vue({
             }else{
                 this.saveResume()
             }
-            //   let User = AV.Object.extend('User')
-            //   let user = new User()
-            //   user.set('resume', this.resume)
-            //   user.save().then(function () {
-            //   }, function (error) {
-            //   });
         },
         saveResume(){}
     }
