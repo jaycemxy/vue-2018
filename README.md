3b7d09d1b609e62aebc66d83c36c153362fd4980
jaycemxy <jayce_ma.xa@foxmail.com>
2018-08-07 13:35:18 +0800

    从数据库保存并获取数据

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # vue-2018
+## 给resume添加一个名为editable-span的组件
+- 通过Vue.component全局注册一个组件，第一个参数是组件名(建议字母全小写且必须包含一个连字符)
+```
+Vue.component('my-component-name', { /* ... */ })
+```
+
+- 组件的data必须是一个函数
+- 每个组件必须只有一个根元素，像这个项目中这样三个元素被包含在一个span的根元素下面
+```
+<span class="editableSpan">
+    <span v-show="!editing">{{value}}</span>
+    <input v-show="editing" type="text" v-bind:value="value" @input="triggerEdit">
+    <button @click="editing = !editing">edit</button>
+</span>
+```
+
 ## 验证用户（邮箱）是否存在
 在methods中有一个onLogin方法，当你填入的邮箱不存在时，error.code === 211
 ![alt text](https://i.loli.net/2018/08/06/5b6856e2bc2f3.png)
@@ -15,6 +31,7 @@ AV对象通过响应头将sessionID传给JS，JS又会将sessionID存入localSto
 当页面初始加载时，会发现注册登录页面闪现，虽然它们一开始默认visible是false的，但由于css的加载比JS要快，所以它们的样式已经加载完成会突然出现一下，随后JS才会将它们隐藏
 
 v-cloak指令隐藏一个一开始不希望它出现的元素
+
 1、在该元素身上添加v-cloak
 
 2、在CSS中添加一个默认隐藏的属性，像下面这样：
@@ -25,3 +42,7 @@ div[v-cloak]{
 ```
 
 3、CSS先加载后该元素被隐藏，随后JS加载，会删掉元素的v-cloak指令，一开始隐藏的元素又显示出来
+## 获取currentUser遇到问题
+问题描述：在登录过后想要拿到当前用户的ID和email时，发现我们无法获得currentUser的这些属性，这是因为vue在深入响应式原理中讲到它不能检测到对象属性的添加或删除，我们必须在data中声明currentUser的ID和email属性
+![alt text](https://i.loli.net/2018/08/07/5b6921b512fa7.jpg)
+![alt text](https://i.loli.net/2018/08/07/5b69212d5a6fa.png)
--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,10 @@ let app = new Vue({
         editingName: false,
         loginVisible: false,
         signUpVisible: false,
+        currentUser: {
+            objectId: undefined,
+            email: ''
+        },
         resume: {
             name: 'yourName',
             weChat: 'weChatID',
@@ -24,12 +28,19 @@ let app = new Vue({
         onEdit(key, value) {
             this.resume[key] = value
         },
-        /* 注册 */
+        /* 判断有没有获取到objectId来判断用户是否登录，从而显示或隐藏登出按钮 */
+        hasLogin(){
+            return !!this.currentUser.objectId
+        },
+        /* 登录 */
         onLogin(e){
             console.log(this.login)
-             AV.User.logIn(this.login.email, this.login.password).then(function (user) {
-                 console.log(user)
-             }, function (error) {
+             AV.User.logIn(this.login.email, this.login.password).then((user)=>{
+                user = user.toJSON()
+                this.currentUser.objectId = user.objectId
+                this.currentUser.email = user.email
+                this.loginVisible =false
+             }, (error)=>{
                 if(error.code === 211){
                     alert('邮箱不存在')
                 }else if(error.code === 210){
@@ -43,15 +54,21 @@ let app = new Vue({
             alert('注销成功')
             window.location.reload()
         },
-        /* 登录 */
+        /* 注册 */
         onSignUp(e){
             const user = new AV.User()
             user.setUsername(this.signUp.email)
             user.setPassword(this.signUp.password)
             user.setEmail(this.signUp.email)
-            user.signUp().then(function (User) {
-                console.log(user)
-            }, function (error) {})
+            user.signUp().then((user) => {
+                alert('注册成功')
+                user = user.toJSON()
+                this.currentUser.objectId = user.objectId
+                this.currentUser.email = user.email
+                this.signUpVisible = false
+            }, (error)=> {
+                alert(error.rawMessage)
+            })
         },
         /* 点击保存按钮 */
         onClickSave(){
@@ -64,10 +81,30 @@ let app = new Vue({
         },
         /* 保存用户数据到leanCloud */
         saveResume(){
-            let {id} = AV.User.current()
-            let user = AV.Object.createWithoutData('User', id)
+            let {objectId} = AV.User.current().toJSON()
+            let user = AV.Object.createWithoutData('User', objectId)
             user.set('resume', this.resume)
-            user.save()
+            user.save().then(()=>{
+                alert('信息保存成功')
+            }, ()=> {
+                alert('信息保存失败')
+            })
+        },
+        /* 从leanCloud获取用户之前编辑过的数据到本地 */
+        getResume(){
+            var query = new AV.Query('User');
+            query.get(this.currentUser.objectId).then((user)=> {
+                let resume = user.toJSON().resume
+                this.resume = resume
+            }, (error)=> {
+                // 异常处理
+            });
         }
     }
 })
+
+let currentUser = AV.User.current()
+if(currentUser){
+    app.currentUser = currentUser.toJSON()
+    app.getResume()
+}
